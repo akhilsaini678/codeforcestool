8759086927d82ad15a7c1c8f9a951ec38ef8c9b3
Akhil Saini <61077340+akhilsaini678@users.noreply.github.com>
2022-02-25 12:39:44 +0530

    Update Contest ID

--- codeforcestool_app/views.py
+++ codeforcestool_app/views.py
@@ -16,10 +16,10 @@ class contest_details():
 # Create your views here.
 def index(request):
     contest_ID = request.POST.get('contest_ID')
-    if contest_ID != None and contest_ID != '' and int(contest_ID)>=1 and int(contest_ID)<=1639:
+    if contest_ID != None and contest_ID != '' and int(contest_ID)>=1 and int(contest_ID)<=1642:
         response=requests.get("https://flaskapitesting.herokuapp.com/api/"+contest_ID)
     else:
-        response=requests.get("https://flaskapitesting.herokuapp.com/api/1634")
+        response=requests.get("https://flaskapitesting.herokuapp.com/api/100")
     print(response.status_code)
     # print(response.json())
     ls = response.json()
@@ -32,10 +32,10 @@ def index(request):
          "problem_name" : ['A','B','C','D','E', 'F', 'G', 'H', 'I', 'J'],
     }
     contest_name = 'Contest ID : '
-    if contest_ID != None and contest_ID != '' and int(contest_ID)>=1 and int(contest_ID)<=1639:
+    if contest_ID != None and contest_ID != '' and int(contest_ID)>=1 and int(contest_ID)<=1642:
         contest_name+=str(contest_ID)
     else:
-        contest_name+='1634'
+        contest_name+='100'
     return render(request,'index.html',{'contest_name':contest_name,'lists':lists})
 
 def profile(request):
